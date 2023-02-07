4cac7735711f49bc8f112e942df703e6b87730c9
Matt <matta628@gmail.com>
2023-02-06 21:28:54 -0500

    Add basic initial structure and style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-const content = document.createElement('div');
-content.innerHTML = 'Todo List Project';
-document.body.appendChild(content);
+import './style.css';
+
+const initPage = () => {
+  const content = document.createElement('div');
+  content.setAttribute('id', 'content');
+  document.body.appendChild(content);
+
+  const header = document.createElement('div');
+  header.classList.add('header');
+  header.innerHTML = '// TODO';
+  content.appendChild(header);
+
+  const main = document.createElement('div');
+  main.classList.add('main');
+  const spotlight = document.createElement('div');
+  spotlight.classList.add('spotlight');
+  const sideProjects = document.createElement('div');
+  sideProjects.classList.add('side-projects');
+  main.appendChild(spotlight);
+  main.appendChild(sideProjects);
+  content.appendChild(main);
+};
+
+initPage();
