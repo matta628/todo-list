7c221b9cbd5b6b5f6c0751087cfcaaceaa5354cf
Matt <matta628@gmail.com>
2023-02-07 11:55:03 -0500

    Remove side project div & refactor page init

--- src/index.js
+++ src/index.js
@@ -1,24 +1,4 @@
 import './style.css';
-
-const initPage = () => {
-  const content = document.createElement('div');
-  content.setAttribute('id', 'content');
-  document.body.appendChild(content);
-
-  const header = document.createElement('div');
-  header.classList.add('header');
-  header.innerHTML = '// TODO';
-  content.appendChild(header);
-
-  const main = document.createElement('div');
-  main.classList.add('main');
-  const spotlight = document.createElement('div');
-  spotlight.classList.add('spotlight');
-  const sideProjects = document.createElement('div');
-  sideProjects.classList.add('side-projects');
-  main.appendChild(spotlight);
-  main.appendChild(sideProjects);
-  content.appendChild(main);
-};
+import initPage from './init';
 
 initPage();
